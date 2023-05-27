61a38cafdd6e44ea6aaad7ebbc587282c5183d8e
Mohammad Saif <msaif@finicity.corp>
2023-05-27 17:17:58 +0530

    initial commit

---
+++ src/main/java/com/exam/service/impl/UserServiceImpl.java
@@ -0,0 +1,62 @@
+package com.exam.service.impl;
+
+import java.util.Set;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.exam.model.User;
+import com.exam.model.UserRole;
+import com.exam.repo.RoleRepository;
+import com.exam.repo.UserRepository;
+import com.exam.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private RoleRepository roleRepository;
+
+	@Override
+	public User createUser(User user, Set<UserRole> userRoles) throws Exception {
+
+		User local = this.userRepository.findByUsername(user.getUsername());
+		if(local != null)
+			try {
+				{
+					System.out.println("User is already there !!");
+					throw new Exception("User already present !!");
+				}
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		else {
+			//user create
+			for(UserRole ur:userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+			user.getUserRoles().addAll(userRoles);
+			local= this.userRepository.save(user);
+		}
+
+		return local;
+	}
+
+	//getting user by username
+	@Override
+	public User getUser(String username) {
+		// TODO Auto-generated method stub
+		return this.userRepository.findByUsername(username);
+	}
+
+	@Override
+	public void deleteUser(Long userId) {
+		// TODO Auto-generated method stub
+		this.userRepository.deleteById(userId);
+	}
+
+}
